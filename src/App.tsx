1dcddd9b647c3e3bc41c3f572168e18414a3c3cf
Jason Lengstorf <jason@lengstorf.com>
2021-03-04 12:44:44 -0800

    feat: migrate CRA to Vite

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Waddup Vite?
         </p>
         <a
           className="App-link"
